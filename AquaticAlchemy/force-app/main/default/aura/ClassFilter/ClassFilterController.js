8730be1d7f9479fb70702666b164fe394ad24a9d
andreology27 <37252145+andreology@users.noreply.github.com>
2021-05-13 16:19:27 -0700

    adding filter to view

--- AquaticAlchemy/force-app/main/default/aura/ClassFilter/ClassFilterController.js
+++ AquaticAlchemy/force-app/main/default/aura/ClassFilter/ClassFilterController.js
@@ -1,5 +1,10 @@
 ({
-    myAction : function(component, event, helper) {
-
+    handleStartFilter : function(component, event, helper) {
+        let classLevels = [{label:'Any', Value:'Any'},
+                             {label:'Beginner', Value:'Beginner'},
+                             {label:'Intermediate', Value:'Intermediate'},
+                             {label:'Advanced', Value:'Advanced'}
+                            ];
+        component.set('v.classLevels', classLevels);
     }
 })
